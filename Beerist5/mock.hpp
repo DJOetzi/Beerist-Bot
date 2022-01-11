a12a5af3c7471b35877a53fda4f962cd87a14ec6
DJ::Ötzi <maxgabri@icloud.com>
2022-01-11 14:40:12 +0100

    mock-update
    simplified mock command

--- Beerist5/mock.hpp
+++ Beerist5/mock.hpp
@@ -19,24 +19,14 @@
 namespace beerist::commands {
 	namespace mock {
 		void exec(dpp::cluster& bot, const dpp::interaction_create_t& event, dpp::command_interaction cmd_data, std::string topgg_link) {
-            std::string basetext = std::get<std::string>(event.get_parameter("to-mock"));
-
-            char* text = (char*)calloc(basetext.length(), sizeof(char));
-            if (text) {
-                strcpy(text, basetext.c_str());
-
-                for (int idx = 0; idx < basetext.length(); idx++) {
-                    text[idx] = (idx % 2 != 0) ? toupper(text[idx]) : tolower(text[idx]);
-                }
-
-                event.reply(dpp::ir_channel_message_with_source, text);
-                free(text);
-                //std::cout << "mem cleared";
+            std::string text = std::get<std::string>(event.get_parameter("to-mock"));
+            int idx = 0;
+            for (char& chr : text) {
+               chr = (idx % 2 != 0) ? toupper(chr) : tolower(chr);
+               idx++;
             }
-            else
-                event.reply(dpp::ir_channel_message_with_source, fmt::format("Error happened while editing `{}`\n**REPORT THIS TO THE DEV IMMEDIATELY**", basetext));
-            /*if (text)
-                free(text);*/
+
+            event.reply(dpp::ir_channel_message_with_source, text);
 		}
    }
 }
